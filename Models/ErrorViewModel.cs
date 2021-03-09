bfa8f48ae2d9500666012a1ab44acca7ae999c15
Wendell Santos <wendellcrifort@gmail.com>
2021-03-09 17:41:08 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Primeiro_app_netcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
